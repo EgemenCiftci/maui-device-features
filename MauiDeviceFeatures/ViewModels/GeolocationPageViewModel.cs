35311a012c31f823ba5aa655b29cd1c5083f2c09
Egemen Çiftci <egemenciftci@gmail.com>
2023-01-04 13:10:58 +0100

    Geolocation finalized.

--- MauiDeviceFeatures/ViewModels/GeolocationPageViewModel.cs
+++ MauiDeviceFeatures/ViewModels/GeolocationPageViewModel.cs
@@ -1,13 +1,67 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MauiDeviceFeatures.ViewModels
 {
     public class GeolocationPageViewModel : ObservableObject
     {
+        private string _lastKnownLocation;
+        public string LastKnownLocation
+        {
+            get => _lastKnownLocation;
+            set => _ = SetProperty(ref _lastKnownLocation, value);
+        }
+
+        private string _currentLocation;
+        public string CurrentLocation
+        {
+            get => _currentLocation;
+            set => _ = SetProperty(ref _currentLocation, value);
+        }
+
+        private bool _isFromMockProvider;
+        public bool IsFromMockProvider
+        {
+            get => _isFromMockProvider;
+            set => _ = SetProperty(ref _isFromMockProvider, value);
+        }
+
+        public GeolocationPageViewModel()
+        {
+            _ = GetLastKnownLocationAsync();
+            _ = GetCurrentLocationAsync();
+        }
+
+        private async Task GetLastKnownLocationAsync()
+        {
+            try
+            {
+                LastKnownLocation = (await Geolocation.Default.GetLastKnownLocationAsync())?.ToString() ?? "None";
+            }
+            catch (Exception ex)
+            {
+                if (Application.Current?.MainPage != null)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+                }
+            }
+        }
+
+        private async Task GetCurrentLocationAsync()
+        {
+            try
+            {
+                Location location = await Geolocation.Default.GetLocationAsync();
+
+                CurrentLocation = location?.ToString() ?? "None";
+                IsFromMockProvider = location != null && location.IsFromMockProvider;
+            }
+            catch (Exception ex)
+            {
+                if (Application.Current?.MainPage != null)
+                {
+                    await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+                }
+            }
+        }
     }
 }
